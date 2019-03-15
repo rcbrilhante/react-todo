894fcee205804041f60a4563ac0a9642d52c9c0c
rcbrilhante <r.brilhante@sapo.pt>
2019-03-15 00:27:26 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import uuid from 'uuid';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import TodoInput from './components/TodoInput';
+import TodoList from './components/TodoList';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <TodoInput />
+        <TodoList />
+      </div>
+    );
+  }
+}
+
+export default App;
