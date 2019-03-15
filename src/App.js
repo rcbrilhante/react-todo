481e62f40f3c94d90f5c7fea3597b8103e67e50c
rcbrilhante <r.brilhante@sapo.pt>
2019-03-15 01:19:59 +0000

    add item to array

--- src/App.js
+++ src/App.js
@@ -5,11 +5,73 @@ import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 
 class App extends Component {
+  state={
+    items:[
+      {
+        id:1,
+        title: 'wake up'
+      },
+      {
+        id:2,
+        title: 'wake up'
+      }
+    ],
+    id: uuid(),
+    item:'',
+    editItem: false
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      item: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id:this.state.id,
+      title:this.state.item
+    }
+    const updatedItem = [...this.state.items, newItem]
+
+    this.setState({
+      items: updatedItem,
+      item: '',
+      id:uuid(),
+      editItem: false
+    })
+  }
+
+  clearList = (e) => {
+    console.log(e);
+  }
+
+  handleDelete = (e) => {
+    console.log(e);
+  }
+
+  handleEdit = (e) => {
+    console.log(e);
+  }
+
   render() {
+    //console.log(this.state, 'asda');
     return (
-      <div className="App">
-        <TodoInput />
-        <TodoList />
+      <div className="container">
+        <div className="row">
+        <TodoInput
+          item={this.state.item}
+          handleChange={this.handleChange}
+          handleSubmit={this.handleSubmit}
+          editItem={this.editItem}/>
+        <TodoList 
+          items={this.state.items}
+          clearList={this.clearList}
+          handleDelete={this.handleDelete}
+          handleEdit={this.handleEdit}
+        />
+        </div>
       </div>
     );
   }
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 
 export default class TodoInput extends Component {
   render() {
+
+    const {item, handleChange, handleSubmit, editItem} = this.props;
+
     return (
       <div>
-        input
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <div className="input-group-text">
+              </div>
+            </div>
+          </div>
+          <input type="text" className="form-control" placeholder="stuff" value={item}
+          onChange={handleChange}/>
+          <button type="submit" className="btn btn-block btn-primary">add</button>
+        </form>
       </div>
     )
   }
